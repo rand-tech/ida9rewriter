79687e7737052df74819d7688a6ef6cba7f33aeb
random <54098069+rand-tech@users.noreply.github.com>
2024-10-16 11:33:32 +0900

    fix

--- ida9rewriter/codemods/replace_simple.py
+++ ida9rewriter/codemods/replace_simple.py
@@ -99,71 +99,36 @@ class IDAAPIUpdater(VisitorBasedCodemodCommand):
 
     def visit_Import(self, node: cst.Import) -> None:
         for alias in node.names:
-            if alias.asname:
-                self.import_aliases[alias.asname.name.value] = alias.name.value
+            asname = alias.asname.name.value if alias.asname else alias.name.value
+            self.import_aliases[asname] = alias.name.value
 
     def visit_ImportFrom(self, node: cst.ImportFrom) -> None:
-        if node.module:
-            if isinstance(node.module, cst.Name):
-                module_name = node.module.value
-            elif isinstance(node.module, cst.Attribute):
-                parts = []
-                attr = node.module
-                while isinstance(attr, cst.Attribute):
-                    parts.append(attr.attr.value)
-                    attr = attr.value
-                if isinstance(attr, cst.Name):
-                    parts.append(attr.value)
-                module_name = ".".join(reversed(parts))
-            else:
-                print(f"Unexpected module type: {type(node.module)}")
-                return
+        module_name = self._get_module_name(node.module) if node.module else ''
+        if isinstance(node.names, cst.ImportStar):
+            self.glob_imports.add(module_name)
+        else:
+            for alias in node.names:
+                asname = alias.asname.name.value if alias.asname else alias.name.value
+                full_name = f"{module_name}.{alias.name.value}".lstrip('.')
+                self.import_aliases[asname] = full_name
 
-            if isinstance(node.names, cst.ImportStar):
-                self.glob_imports.add(module_name)
-            else:
-                for alias in node.names:
-                    self.import_aliases[alias.name.value] = f"{module_name}.{alias.name.value}"
+    def leave_Attribute(self, original_node: cst.Attribute, updated_node: cst.Attribute) -> cst.BaseExpression:
+        full_name = self._get_full_name(updated_node)
+        if (new_full_name := REPLACEMENTS.get(full_name)):
+            return self._construct_attribute(new_full_name)
+        return updated_node
 
     def leave_Call(self, original_node: cst.Call, updated_node: cst.Call) -> cst.Call:
+        full_name = None
         if isinstance(updated_node.func, cst.Attribute):
             full_name = self._get_full_name(updated_node.func)
-            if full_name in REPLACEMENTS or self._check_glob_imports(full_name):
-                new_func = REPLACEMENTS.get(full_name, full_name)
-                return self._replace_func(updated_node, new_func)
         elif isinstance(updated_node.func, cst.Name):
             func_name = updated_node.func.value
             full_name = self.import_aliases.get(func_name, func_name)
-            if full_name in REPLACEMENTS or self._check_glob_imports(full_name):
-                new_func = REPLACEMENTS.get(full_name, full_name)
-                return self._replace_func(updated_node, new_func)
+
+        if (new_func := REPLACEMENTS.get(full_name)):
+            return self._replace_func(updated_node, new_func)
         return updated_node
-
-    def _check_glob_imports(self, full_name: str) -> bool:
-        for glob_import in self.glob_imports:
-            if full_name.startswith(glob_import):
-                return True
-        return False
-
-    def _get_full_name(self, node: cst.Attribute) -> str:
-        parts = []
-        while isinstance(node, cst.Attribute):
-            parts.append(node.attr.value)
-            node = node.value
-        if isinstance(node, cst.Name):
-            parts.append(node.value)
-        full_name = ".".join(reversed(parts))
-        return self.import_aliases.get(full_name.split(".")[0], full_name)
-
-    def _replace_func(self, node: cst.Call, new_func: str) -> cst.Call:
-        parts = new_func.split(".")
-        if len(parts) > 1:
-            value = cst.Name(parts[0])
-            for part in parts[1:-1]:
-                value = cst.Attribute(value=value, attr=cst.Name(part))
-            return node.with_changes(func=cst.Attribute(value=value, attr=cst.Name(parts[-1])))
-        else:
-            return node.with_changes(func=cst.Name(new_func))
 
     def leave_FunctionDef(self, original_node: cst.FunctionDef, updated_node: cst.FunctionDef) -> cst.FunctionDef:
         if updated_node.name.value == "construct_macro":
@@ -175,6 +140,42 @@ class IDAAPIUpdater(VisitorBasedCodemodCommand):
             return updated_node.with_changes(params=cst.Parameters(new_params), returns=cst.Annotation(cst.Name("bool")))
         return updated_node
 
+    def _get_module_name(self, module: cst.BaseExpression) -> str:
+        if isinstance(module, cst.Name):
+            return module.value
+        elif isinstance(module, cst.Attribute):
+            return self._get_full_name(module)
+        return ''
+
+    def _get_full_name(self, node: cst.Attribute|cst.Name) -> str:
+        assert isinstance(node, cst.Attribute) or isinstance(node, cst.Name), f"Expected Attribute, got {type(node)} instead"
+        parts = []
+        while isinstance(node, cst.Attribute):
+            parts.append(node.attr.value)
+            node = node.value
+        if isinstance(node, cst.Name):
+            parts.append(node.value)
+        else:
+            return ""
+        assert all(isinstance(part, str) for part in parts)
+        full_name = parts[::-1]
+        resolved_first_part = self.import_aliases.get(full_name[0], '.'.join(full_name))
+        if not isinstance(resolved_first_part, str):
+            resolved_first_part = self._get_full_name(resolved_first_part)
+        full_name = [resolved_first_part] + list(full_name)[1:]
+        return '.'.join(full_name)
+
+    @staticmethod
+    def _construct_attribute(full_name: str) -> cst.BaseExpression:
+        parts = full_name.split(".")
+        node = cst.Name(parts[0])
+        for part in parts[1:]:
+            node = cst.Attribute(value=node, attr=cst.Name(part))
+        return node
+
+    def _replace_func(self, node: cst.Call, new_func: str) -> cst.Call:
+        new_func_node = self._construct_attribute(new_func)
+        return node.with_changes(func=new_func_node)
 
 def bump_ida_simple(source_code: str) -> str:
     module = cst.parse_module(source_code)
